46c8ad7a7da7c97e8fad24640ac65163b353dd59
Caleb-Andreas <caleb.andreas@mths.ca>
2024-09-17 11:30:25 -0400

    Made code with basic math question

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Sep 2024
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A rectangle has dimentions 5 cm & 3 cm.')
+basic.pause(500)
+
+// multiply
+basic.showString('The area would be:' + (5 * 3).toString() + 'cm^2.')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
+
+basic.pause(500)
+
+// multiply
+basic.showString('The perimiter would be:' + (2 * (5 + 3)).toString() + 'cm.')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
