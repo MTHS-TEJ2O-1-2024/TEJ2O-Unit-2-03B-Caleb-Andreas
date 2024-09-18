501f18b744a525f971ea944fbb56de33445a4f29
Caleb-Andreas <caleb.andreas@mths.ca>
2024-09-18 10:57:51 -0400

    fixed spelling errors

--- main.ts
+++ main.ts
@@ -8,11 +8,11 @@
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString('A rectangle has dimentions 5 cm & 3 cm.')
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
 basic.pause(500)
 
 // multiply
-basic.showString('The perimiter would be:' + (2 * (5 + 3)).toString() + 'cm.')
+basic.showString('The perimeter would be:' + (2 * (5 + 3)).toString() + 'cm.')
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
