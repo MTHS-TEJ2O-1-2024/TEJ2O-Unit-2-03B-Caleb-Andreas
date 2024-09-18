6083f9d0cb0f55fcc45f7f5fc728105dcfa6c406
Caleb-Andreas <caleb.andreas@mths.ca>
2024-09-18 10:54:51 -0400

    Changed the order the multiplications are in

--- main.ts
+++ main.ts
@@ -12,15 +12,13 @@ basic.showString('A rectangle has dimentions 5 cm & 3 cm.')
 basic.pause(500)
 
 // multiply
-basic.showString('The area would be:' + (5 * 3).toString() + 'cm^2.')
+basic.showString('The perimiter would be:' + (2 * (5 + 3)).toString() + 'cm.')
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
 
-basic.pause(500)
-
 // multiply
-basic.showString('The perimiter would be:' + (2 * (5 + 3)).toString() + 'cm.')
+basic.showString('The area would be:' + (5 * 3).toString() + 'cm^2.')
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
